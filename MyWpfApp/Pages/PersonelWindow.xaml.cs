9a72b1a26f8f21521239275cbd0b1448c4ab4df1
yuceluyan <49283796+yuceluyan@users.noreply.github.com>
2020-04-30 15:21:35 +0300

    Add files via upload

---
+++ MyWpfApp/Pages/PersonelWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using MyWpfApp.PersonelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWpfApp.Pages
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+     
+    public partial class PersonelWindow : Window
+    {
+        private static readonly Regex regex = new Regex("[^0-9.-]+");
+        public List<Employee> PersonelList { get; set; }
+        public Employee SelectedPersonel { get; set; }
+        
+        public PersonelWindow()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+        private void LoadGrid()
+        {
+            PersonelServiceClient con = new PersonelServiceClient();
+            PersonelList = con.getPersonelList().ToList();
+        }
+        private void BtnEkle_Click(object sender, RoutedEventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = txtAd.Text;
+            emp.LastName = txtSoyAd.Text;
+            emp.HomePhone = txtTelefon.Text;
+            emp.Address = txtAdres.Text;
+
+            if (emp!=null)
+            {
+                PersonelServiceClient con = new PersonelServiceClient();
+                con.Ekle(emp);
+            }
+
+          
+        }
+
+        private void BtnSil_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedPersonel == null)
+            {
+                MessageBox.Show("Personel seçiniz..");
+                return;
+            }
+
+            PersonelServiceClient con = new PersonelServiceClient();
+            con.Sil(SelectedPersonel);
+        }
+
+        private void BtnGuncelle_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedPersonel == null)
+            {
+                MessageBox.Show("Personel seçiniz..");
+                return;
+            }
+
+            Employee emp = new Employee();
+            emp.FirstName = txtAd.Text;
+            emp.LastName = txtSoyAd.Text;
+            emp.HomePhone = txtTelefon.Text;
+            emp.Address = txtAdres.Text;
+
+            PersonelServiceClient con = new PersonelServiceClient();
+            con.Guncelle(emp);
+        }
+
+        private void BtnTemizle_Click(object sender, RoutedEventArgs e)
+        {
+            txtAd.Text = "";
+            txtSoyAd.Text = "";
+            txtTelefon.Text = "";
+            txtAdres.Text = "";
+        }
+
+        private void BtnAra_Click(object sender, RoutedEventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = txtAd.Text;
+            emp.LastName = txtSoyAd.Text;
+
+            if (emp!=null)
+            {
+                PersonelServiceClient con = new PersonelServiceClient();
+                PersonelList = con.Ara(emp).ToList();
+            }
+
+        }
+
+        private void TxtTelefon_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            return !regex.IsMatch(text);
+        }
+
+
+    }
+}
